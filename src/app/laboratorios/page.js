2140896530b8834ed0eba9ccbb5a0b0316162438
yanditv <yankur44@gmail.com>
2023-08-09 11:59:04 -0500

    updated

---
+++ src/app/laboratorios/page.js
@@ -0,0 +1,7 @@
+export default function Laboratorios() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-40">
+      <h1>Laboratorios</h1>
+    </main>
+  );
+}
