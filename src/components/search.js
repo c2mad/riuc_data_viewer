838448d77436dff74d6f7e5cc3472b406061a5b6
Gustavo-Lozada <119980944+Gustavo-Lozada@users.noreply.github.com>
2023-08-14 12:54:34 -0500

    serach actualizado

--- src/components/search.js
+++ src/components/search.js
@@ -50,7 +50,7 @@ export default function Search() {
 
 return (
     <div className="w-full">
-    <div className="relative">
+    <div className="">
         <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
         <svg
             className="w-5 h-5 text-gray-400"
