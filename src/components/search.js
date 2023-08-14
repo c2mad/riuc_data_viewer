040278157ab4cc33c09206efd33a73a07becee17
Gustavo-Lozada <119980944+Gustavo-Lozada@users.noreply.github.com>
2023-08-14 13:02:41 -0500

    serach

--- src/components/search.js
+++ src/components/search.js
@@ -52,20 +52,7 @@ return (
     <div className="w-full">
     <div className="">
         <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-        <svg
-            className="w-5 h-5 text-gray-400"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-        >
-            <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={2}
-            d="M21 21l-4.35-4.35"
-            />
-            <circle cx={11} cy={11} r={8} />
-        </svg>
+            
         </div>
         <input
         className="block w-full py-2 pl-10 pr-3 text-sm text-black placeholder-gray-400 bg-white border border-black rounded-md shadow-sm focus:outline-none focus:ring-1 focus:ring-red-500 focus:border-black sm:text-sm"
