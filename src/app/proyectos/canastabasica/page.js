29e03e40d4b922796301d6df2d7ef7c3baddb210
Gustavo-Lozada <119980944+Gustavo-Lozada@users.noreply.github.com>
2023-08-30 11:06:14 -0500

    proyect

--- src/app/proyectos/canastabasica/page.js
+++ src/app/proyectos/canastabasica/page.js
@@ -25,15 +25,18 @@ export default function canastabasica() {
   return (
     <main className="scroll-behavior-smooth text-sm"> 
 
-  
+      {/* Informacion del proyecto */}
       <div className="flex items-center justify-center h-80 overflow-hidden bg-cover bg-center bg-no-repeat"
        style={{ backgroundImage: "url('/img/geovisor_mapa.png')",
       }}>
       
       <div className="p-5">
-        <h1 className="text-4xl font-bold text-white mb-4">Geovisor de Canasta Basica</h1>
-        <p className="text-lg text-white">
-          Descripción del proyecto.
+        <h1 className="text-4xl font-bold text-black mb-4">Geovisor de Canasta Basica</h1>
+        <p className="text-lg text-black">
+        Este proyecto consiste en una herramienta interactiva y visualmente impactante que tiene como objetivo principal 
+        proporcionar una perspectiva detallada y accesible sobre los datos relacionados con la canasta básica en el país de Ecuador a lo largo de diversos años.
+        Este geovisor aprovecha las capacidades de la tecnología geoespacial y la visualización de datos para ofrecer a los usuarios una comprensión profunda de cómo los precios 
+        y la disponibilidad de los elementos esenciales en la canasta básica han evolucionado con el tiempo en diferentes regiones del país.
         </p>
       </div>
     </div>
--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -29,6 +29,11 @@ export default function SideBar({ navLinks }) {
     setOpen(false);
   }
 
+  function proyect(to){
+    router.push(to);
+    setOpen(false);
+  }
+
   return (
     <>
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
@@ -127,9 +132,12 @@ export default function SideBar({ navLinks }) {
                         id="menu1"
                         class="flex justify-start  flex-col w-full md:w-auto items-start pb-1 "
                       >
+                        {/* Agrega la opción "Ver todos los proyectos" */}
+                        <button onClick={() => proyect("/proyectos")} className="flex justify-start items-center space-x-6 hover:text-white focus:bg-red-800 focus:text-white hover:bg-red-800 text-gray-400 rounded px-3 py-2  w-full md:w-52">Ver todos los proyectos</button>
                         {lista_proyectos.map((proyecto) => (
                           <button key={proyecto.id} onClick={()=>goto(proyecto.to)} class="flex justify-start items-center space-x-6 hover:text-white focus:bg-red-800 focus:text-white hover:bg-red-800 text-gray-400 rounded px-3 py-2  w-full md:w-52">
-                            <p class="text-base leading-6  ">{proyecto.name}</p>
+                            <p class="text-base leading-6  ">- {proyecto.name}</p>
+
                           </button>
                         ))}
                       </div>
@@ -141,14 +149,15 @@ export default function SideBar({ navLinks }) {
               </>
             );
           })}
-        <div className="absolute bottom-0 w-full">
+           <div className="absolute bottom-0 w-full">
           <h1 className="font-sans text-center text-white">
             ciitt@ucacue.edu.ec
+
           </h1>
         </div>
         </div>
+        </div>
         
-      </div>
     </>
   );
 }
