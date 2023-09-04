2b9076589e7cf17415b077eb1f09ff6f37f4fed7
Gustavo-Lozada <119980944+Gustavo-Lozada@users.noreply.github.com>
2023-09-04 14:53:05 -0500

    redes sociales agregadas en footer

--- src/components/buttonswitch.js
+++ src/components/buttonswitch.js
@@ -1,18 +1,21 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const SwitchButton = ({ isActive, onClick }) => {
   const getStatus = () => {
-    return isActive ? ' On' : ' Off';
+    return isActive ? 'On' : 'Off';
   };
 
+  const buttonClasses = `rounded-full w-8 h-6 transition-colors duration-300 focus:outline-none block ${
+    isActive ? 'bg-green-400' : 'bg-gray-200'
+  }`;
+
   return (
-    <div className="">
+    <div className="flex items-center">
       <button
-        className={`${
-          isActive ? 'bg-green-400' : 'bg-gray-200'
-        } items-center h-6 rounded-full w-8 transition-colors duration-300 focus:outline-none block`}
+        className={buttonClasses}
         onClick={onClick}
-        style={{ zIndex: 1 }}
+        aria-label={`Turn ${getStatus()} the switch`}
       >
         <svg
           xmlns="http://www.w3.org/2000/svg"
@@ -26,9 +29,14 @@ const SwitchButton = ({ isActive, onClick }) => {
           <path d="M3 8.812a4.999 4.999 0 0 1 2.578-4.375l-.485-.874A6 6 0 1 0 11 3.616l-.501.865A5 5 0 1 1 3 8.812z" />
         </svg>
       </button>
-      <span>{getStatus()}</span>
+      <span className="ml-2">{getStatus()}</span>
     </div>
   );
+};
+
+SwitchButton.propTypes = {
+  isActive: PropTypes.bool.isRequired,
+  onClick: PropTypes.func.isRequired,
 };
 
 export default SwitchButton;
