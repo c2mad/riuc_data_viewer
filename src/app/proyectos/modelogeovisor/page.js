a1a54d0d82ee6c010d1d5319a4fbc19d913e4330
Gustavo-Lozada <119980944+Gustavo-Lozada@users.noreply.github.com>
2023-09-14 11:19:38 -0500

    geovisor modificado

--- src/app/proyectos/modelogeovisor/page.js
+++ src/app/proyectos/modelogeovisor/page.js
@@ -23,31 +23,31 @@ export default function modelogeovisor() {
   return (
     <main style={{ scrollBehavior: "smooth" }}>
       <div className="flex space-x-3 p-3 font-">
-        <div className="h-[calc(100vh-_80px)] w-[400px] overflow-hidden rounded-lg bg-white p-3 shadow-md">
+        <div className="h-[calc(100vh-_80px)] w-[410px] overflow-hidden rounded-lg bg-white p-3 shadow-md">
           {/* Contenido de la primera columna */}
           <div className="flex">
             <div className="w-[400px]">
-              <div className="h-[calc(100vh-80px)] overflow-hidden overflow-y-auto rounded-lg bg-white p-3 shadow-md">
-                {/* Botón para regresar a la página anterior */}
-                <button
-                  className="p-2 text-gray-500 hover:underline mb-2 font-medium mb-2 flex items-center space-x-2"
-                  onClick={() => proyect("/proyectos")}
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="16"
-                    height="16"
-                    fill="currentColor"
-                    class="bi bi-arrow-left-circle"
-                    viewBox="0 0 16 16"
-                  >
-                    <path
-                      fill-rule="evenodd"
-                      d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-4.5-.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z"
-                    />
-                  </svg>
-                  Regresar
-                </button>
+            <div className="flex flex-col">
+            <button
+              className="p-2 text-gray-500 hover:underline mb-2 font-medium mb-2 flex items-center space-x-2"
+              onClick={() => proyect("/proyectos")}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                className="bi bi-arrow-left-circle text-red-400"
+                viewBox="0 0 16 16"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-4.5-.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z"
+                />
+              </svg><span className="text-gray-500 hover:text-red-400 transition">
+                Regresar
+              </span>
+            </button>
 
                 <div className="border-b border-gray-300 pb-1">
                   <h2 className="text-base font-medium text-black mb-1">
@@ -175,7 +175,7 @@ export default function modelogeovisor() {
             </div>
           </div>
         </div>
-        <div className="h-[calc(100vh-_80px)] w-[400px] overflow-hidden overflow-y-auto rounded-lg bg-white p-3 shadow-md">
+        <div className="h-[calc(100vh-_80px)] w-[300px] overflow-hidden overflow-y-auto rounded-lg bg-white p-3 shadow-md">
           {/* Contenido de la tercera columna */}
           <div className="border-b border-gray-300 pb-1">
             <h3 className="text-xl font-semibold text-slate-700">Tablas</h3>
