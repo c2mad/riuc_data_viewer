0ce164576eb5594c68dbaea5f4ce60812df49791
Gustavo-Lozada <119980944+Gustavo-Lozada@users.noreply.github.com>
2023-10-03 09:58:52 -0500

    boletin modificado

--- src/app/boletin/n1/page.js
+++ src/app/boletin/n1/page.js
@@ -1,16 +1,44 @@
 "use client";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 export default function boletinn1() {
+  const router = useRouter();
   const abrirPDF = () => {
     // Reemplaza 'nombre-del-archivo.pdf' con el nombre real de tu archivo PDF
     const pdfUrl = "/pdf/REVISTA.pdf";
     window.open(pdfUrl, "_blank");
   };
 
+  function proyect(to) {
+    router.push(to);
+  }
+
   return (
     <div className="bg-white p-6 mx-auto max-w-7xl font-serif ">
       <div className="bg-white p-6 mx-auto max-w-7xl font-serif">
-        <h1 className="text-6xl text-red-500 font-extrabold mb-5 mt-8">
+      <button
+                  className="p-1 text-gray-500 hover:underline mb-2 font-medium mb-2 flex items-center space-x-2 mt-5"
+                  onClick={() => proyect("/boletin")}
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    fill="currentColor"
+                    className="bi bi-arrow-left-circle text-red-400"
+                    viewBox="0 0 16 16"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-4.5-.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z"
+                    />
+                  </svg>
+                  <span className="text-gray-500 hover:text-red-400 transition">
+                    Regresar
+                  </span>
+                </button>
+        <h1 className="text-6xl text-red-500 font-extrabold mb-5 mt-3">
           BOLETIN
         </h1>
         <h2 className="text-base font-medium text-gray-400 mb-1">
@@ -24,46 +52,48 @@ export default function boletinn1() {
         vida en la Universidad Católica de Cuenca. Este boletín está conformado
         por cuatro secciones:
       </p>
-      <div className="text-lg text-gray-800 mb-5 text-justify">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-5 w-5 inline text-red-500 mr-2"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="M5 13l4 4L19 7"
-          />
-        </svg>
-        La primera denominada, Bajo la lupa de los observatorios, presenta en
-        qué consiste cada observatorio, quienes son los responsables, qué
-        servicios ofrecen y cuáles son los proyectos actuales que llevan
-        adelante.
-      </div>
-      <div className="text-lg text-gray-800 mb-5 text-justify">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-5 w-5 inline text-red-500 mr-2"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="M5 13l4 4L19 7"
-          />
-        </svg>
-        La segunda sección, denominada “hablando con expertos”, pretende
-        presentar la opinión de investigadores en diferentes áreas de la ciencia
-        haciendo énfasis en sus respectivas especialidades de investigación.
-      </div>
-      <div className="text-lg text-gray-800 mb-5 text-justify">
+      <div className="flex">
+  <div className="flex-1">
+    <div className="text-lg text-gray-800 mb-5 text-justify">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="h-5 w-5 inline text-red-500 mr-2"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="2"
+          d="M5 13l4 4L19 7"
+        />
+      </svg>
+      La primera denominada, Bajo la lupa de los observatorios, presenta en
+      qué consiste cada observatorio, quienes son los responsables, qué
+      servicios ofrecen y cuáles son los proyectos actuales que llevan
+      adelante.
+    </div>
+    <div className="text-lg text-gray-800 mb-5 text-justify">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="h-5 w-5 inline text-red-500 mr-2"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="2"
+          d="M5 13l4 4L19 7"
+        />
+      </svg>
+      La segunda sección, denominada “hablando con expertos”, pretende
+      presentar la opinión de investigadores en diferentes áreas de la ciencia
+      haciendo énfasis en sus respectivas especialidades de investigación.
+    </div>
+    <div className="text-lg text-gray-800 mb-5 text-justify">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           className="h-5 w-5 inline text-red-500 mr-2"
@@ -82,6 +112,18 @@ export default function boletinn1() {
         investigaciones, así como mostrar las bases de datos que se manejan con
         información de calidad.
       </div>
+  </div>
+  <div className="flex-1">
+    <Image
+      className="mx-auto rounded-lg"
+      src="/img/phd_boletin.png"
+      alt="RIOUC"
+      width={400}
+      height={175}
+    />
+  </div>
+</div>
+
       <div className="text-lg text-gray-800 mb-10 text-justify">
         <svg
           xmlns="http://www.w3.org/2000/svg"
