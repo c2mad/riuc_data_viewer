d9d95b50b46ebb67dbbafafc4ad873f0fae1e36a
Gustavo-Lozada <119980944+Gustavo-Lozada@users.noreply.github.com>
2024-06-19 09:12:19 -0500

    updated

--- src/app/api/auth/[...nextauth]/route.tsx
+++ src/app/api/auth/[...nextauth]/route.tsx
@@ -5,6 +5,12 @@ const handler = NextAuth({
   providers: [ GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+      // Fuerza la selección de cuenta cada vez que se inicia sesión
+      authorization: {
+        params: {
+          prompt: "select_account", 
+        },
+      },
     }),
   ],
 });
