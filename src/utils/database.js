ff76506f122482c5bc6a634e3fe6b8274997bff8
Gustavo-Lozada <119980944+Gustavo-Lozada@users.noreply.github.com>
2024-08-01 14:21:40 -0500

    puerto_db

--- src/utils/database.js
+++ src/utils/database.js
@@ -1,6 +1,6 @@
 const pgp = require('pg-promise')();
 
-const connectionString = 'postgresql://postgres:riouc@192.168.10.11:5432/riouc';
+const connectionString = 'postgresql://postgres:riouc@192.168.10.11:8484/riouc';
 
 const db = pgp(connectionString);
 
